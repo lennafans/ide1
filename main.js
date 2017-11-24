9919a4e02a78ceced75e21c52a296f0c6d0c67b0
Jens Egholm <jensegholm@protonmail.com>
2017-11-24 15:52:16 +0100

    Added neuron model

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 d3.select(window).on('load', () => {
-  console.log(d3.select("body"));
-  d3.select("body")
-    .append('div')
+  d3.select("#images")
     .append('img')
     .attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Caf%C3%A9_wall.svg/840px-Caf%C3%A9_wall.svg.png")
     .attr("alt", "Café wall");
